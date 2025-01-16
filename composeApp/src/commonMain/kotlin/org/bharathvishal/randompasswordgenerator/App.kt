8cd3d7bb40169f2e750b9fc8901ca71ff20e090d
Bharath Vishal G <bharathvishalg@gmail.com>
2025-01-16 09:37:57 +0530

    dev commit

--- composeApp/src/commonMain/kotlin/org/bharathvishal/randompasswordgenerator/App.kt
+++ composeApp/src/commonMain/kotlin/org/bharathvishal/randompasswordgenerator/App.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,11 +17,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemGestures
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Divider
+import androidx.compose.material.Scaffold
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -29,6 +33,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
@@ -39,6 +45,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -46,6 +53,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.bharathvishal.biometricauthentication.theme.AppTheme
+import kotlinx.coroutines.launch
 import org.bharathvishal.randompasswordgenerator.Constants.Constants
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -55,6 +63,7 @@ import randompasswordgenerator.composeapp.generated.resources.baseline_password_
 private var showSnackBarVal = mutableStateOf(false)
 private var snackBarMessageVal = mutableStateOf("-")
 private var passwordTextVal = mutableStateOf("ABCDEFGHIJK")
+private var passwordLengthVal = mutableStateOf(9)
 val mCheckedStateCapitalLetters = mutableStateOf(false)
 val mCheckedStateSmallLetters = mutableStateOf(false)
 val mCheckedStateNumbers = mutableStateOf(false)
@@ -63,16 +72,13 @@ val mCheckedStateSymbol = mutableStateOf(false)
 @Composable
 @Preview
 fun App(
-    darkTheme: Boolean,
-    dynamicColor: Boolean
+    darkTheme: Boolean, dynamicColor: Boolean
 ) {
     AppTheme(
-        darkTheme = darkTheme,
-        dynamicColor = dynamicColor
+        darkTheme = darkTheme, dynamicColor = dynamicColor
     ) {
         Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
+            modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
         ) {
             MainViewImplementation()
         }
@@ -83,23 +89,29 @@ fun App(
 //Adds scroll view to compos
 @Composable
 fun MainViewImplementation() {
-    Column(
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
+    Scaffold(topBar = { TopAppBarMain() },
+        snackbarHost = { SnackbarHost(snackbarHostState) }
     ) {
-        TopAppBarMain()
-        Box(
-            modifier = Modifier
-                .padding(1.dp)
-                .fillMaxWidth()
-                .fillMaxHeight()
+        Column(
         ) {
-            CardViewMain()
             Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight()
-                    .align(Alignment.BottomStart),
+                modifier = Modifier.padding(1.dp).fillMaxWidth().fillMaxHeight()
             ) {
-                SnackBarViewComposable(showSnackBarVal.value, snackBarMessageVal.value)
+                CardViewMain()
+                Box(
+                    modifier = Modifier.fillMaxWidth().wrapContentHeight()
+                        .align(Alignment.BottomStart)
+                ) {
+                    scope.launch {
+                        snackbarHostState.showSnackbar(
+                            "Snackbar Test"
+                        )
+                    }
+                    //SnackBarViewComposable(showSnackBarVal.value, snackBarMessageVal.value)
+                }
             }
         }
     }
@@ -124,37 +136,31 @@ fun CardViewMain() {
     ) {
         Spacer(modifier = Modifier.padding(top = 6.dp))
         Card(
-            modifier = Modifier
-                .padding(10.dp)
-                .fillMaxWidth()
-                .wrapContentHeight().verticalScroll(rememberScrollState()),
+            modifier = Modifier.padding(10.dp).fillMaxWidth().wrapContentHeight()
+                .verticalScroll(rememberScrollState()),
             colors = CardDefaults.cardColors(),
             shape = RoundedCornerShape(16.dp)
         ) {
             Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxWidth()
-                    .wrapContentHeight(),
+                modifier = Modifier.padding(16.dp).fillMaxWidth().wrapContentHeight(),
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally,
-            )
-            {
+            ) {
                 ImageLogo()
                 TextHeader()
                 Divider(thickness = 0.5.dp)
                 RowComponentPasswordLabel(passwordTextVal.value)
                 RowComponentButtons()
                 Divider(thickness = 0.5.dp)
-                RowComponentInCardSlider("Password Length")
+                RowComponentInCardSlider(Constants.PASSWORD_LENGTH)
                 Divider(thickness = 0.5.dp)
                 RowComponentInCardSwitch(Constants.INCLUDE_CAPITAL_LETTERS)
                 Divider(thickness = 0.5.dp)
-                RowComponentInCardSwitch("Include small letters (a ,b, c)")
+                RowComponentInCardSwitch(Constants.INCLUDE_SMALL_LETTERS)
                 Spacer(modifier = Modifier.padding(top = 6.dp))
-                RowComponentInCardSwitch("Include numbers (1 ,2, 3)")
+                RowComponentInCardSwitch(Constants.INCLUDE_NUMBERS)
                 Spacer(modifier = Modifier.padding(top = 6.dp))
-                RowComponentInCardSwitch("Include symbols (@ ,!, %)")
+                RowComponentInCardSwitch(Constants.INCLUDE_SYMBOLS)
                 Spacer(modifier = Modifier.padding(top = 6.dp))
             }//end of column
         }//end of card
@@ -167,10 +173,7 @@ fun ImageLogo() {
     Image(
         painter = painterResource(Res.drawable.baseline_password_24),
         contentDescription = "Image Logo",
-        modifier = Modifier
-            .requiredHeight(125.dp)
-            .requiredWidth(125.dp)
-            .padding(5.dp)
+        modifier = Modifier.requiredHeight(125.dp).requiredWidth(125.dp).padding(5.dp)
     )
 }
 
@@ -180,9 +183,7 @@ fun TextHeader() {
     Text(
         text = "Random Password Generator",
         textAlign = TextAlign.Center,
-        modifier = Modifier
-            .padding(5.dp)
-            .fillMaxWidth(),
+        modifier = Modifier.padding(5.dp).fillMaxWidth(),
         style = MaterialTheme.typography.headlineSmall
     )
 }
@@ -190,7 +191,9 @@ fun TextHeader() {
 @Composable
 fun SnackBarViewComposable(visibilityState: Boolean, message: String) {
     AnimatedVisibility(visible = visibilityState) {
-        Snackbar(action = {}) {
+        Snackbar(
+            action = {}
+        ) {
             Text(text = message)
         }
     }
@@ -200,27 +203,24 @@ fun SnackBarViewComposable(visibilityState: Boolean, message: String) {
 //Row component composable function for Biometric related info
 @Composable
 fun RowComponentInCardSlider(strDesc: String) {
-    var sliderPosition by remember { mutableFloatStateOf(0f) }
+    var sliderPosition by remember { mutableFloatStateOf(9f) }
 
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center
     ) {
         Text(
             text = strDesc,
             textAlign = TextAlign.Center,
-            modifier = Modifier
-                .padding(5.dp)
-                .fillMaxWidth(),
+            modifier = Modifier.padding(5.dp).fillMaxWidth(),
             style = MaterialTheme.typography.labelLarge,
             color = MaterialTheme.colorScheme.primary
         )
 
         Slider(
-            value = sliderPosition.toFloat(),
-            onValueChange = { sliderPosition = it.toInt().toFloat() },
-            steps = 30,
-            valueRange = 9f..30f
+            value = sliderPosition.toFloat(), onValueChange = {
+                sliderPosition = it.toInt().toFloat()
+                passwordLengthVal.value = it.toInt()
+            }, steps = 30, valueRange = 9f..30f
         )
         Text(text = sliderPosition.toInt().toString())
     }
@@ -231,44 +231,45 @@ fun RowComponentInCardSlider(strDesc: String) {
 fun RowComponentInCardSwitch(strDesc: String) {
     val mCheckedState = remember { mutableStateOf(false) }
 
-    if(strDesc.)
+    when (strDesc) {
+        Constants.INCLUDE_CAPITAL_LETTERS -> mCheckedStateCapitalLetters.value = mCheckedState.value
+        Constants.INCLUDE_SMALL_LETTERS -> mCheckedStateSmallLetters.value = mCheckedState.value
+        Constants.INCLUDE_NUMBERS -> mCheckedStateNumbers.value = mCheckedState.value
+        Constants.INCLUDE_SYMBOLS -> mCheckedStateSymbol.value = mCheckedState.value
+    }
 
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center
     ) {
         Text(
             text = strDesc,
             textAlign = TextAlign.Center,
-            modifier = Modifier
-                .padding(5.dp)
-                .fillMaxWidth(),
+            modifier = Modifier.padding(5.dp).fillMaxWidth(),
             style = MaterialTheme.typography.labelLarge,
             color = MaterialTheme.colorScheme.primary
         )
 
-        Switch(checked = mCheckedState.value,
-            onCheckedChange = { mCheckedState.value = it })
+        Switch(checked = mCheckedState.value, onCheckedChange = { mCheckedState.value = it })
     }
 }
 
 
 @Composable
 fun RowComponentButtons() {
+    val scope = rememberCoroutineScope()
+
     Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(4.dp),
+        modifier = Modifier.fillMaxWidth().padding(4.dp),
         horizontalArrangement = Arrangement.spacedBy(4.dp)
     ) {
         OutlinedButton(
             modifier = Modifier.weight(1F),
-            onClick = { },
+            onClick = {
+                showSnackBarVal.value = true
+                snackBarMessageVal.value = "Generated Random password"
+            },
             contentPadding = PaddingValues(
-                start = 20.dp,
-                top = 12.dp,
-                end = 20.dp,
-                bottom = 12.dp
+                start = 20.dp, top = 12.dp, end = 20.dp, bottom = 12.dp
             ),
         ) {
             Spacer(Modifier.size(ButtonDefaults.IconSpacing))
@@ -277,12 +278,12 @@ fun RowComponentButtons() {
 
         OutlinedButton(
             modifier = Modifier.weight(1F),
-            onClick = { },
+            onClick = {
+                showSnackBarVal.value = true
+                snackBarMessageVal.value = "Copied Password"
+            },
             contentPadding = PaddingValues(
-                start = 20.dp,
-                top = 12.dp,
-                end = 20.dp,
-                bottom = 12.dp
+                start = 20.dp, top = 12.dp, end = 20.dp, bottom = 12.dp
             ),
         ) {
             Spacer(Modifier.size(ButtonDefaults.IconSpacing))
@@ -295,9 +296,7 @@ fun RowComponentButtons() {
 @Composable
 fun RowComponentPasswordLabel(strPass: String) {
     Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
+        modifier = Modifier.fillMaxWidth().padding(8.dp),
         horizontalArrangement = Arrangement.spacedBy(4.dp)
     ) {
         Text(
--- composeApp/src/commonMain/kotlin/org/bharathvishal/randompasswordgenerator/Constants/Constants.kt
+++ composeApp/src/commonMain/kotlin/org/bharathvishal/randompasswordgenerator/Constants/Constants.kt
@@ -2,9 +2,9 @@ package org.bharathvishal.randompasswordgenerator.Constants
 
 
 object Constants {
-    const val PASSWORD_LENGTH = "PAssword Length")"
+    const val PASSWORD_LENGTH = "Password Length"
     const val INCLUDE_CAPITAL_LETTERS = "Include capital letters (A ,B, C)"
     const val INCLUDE_SMALL_LETTERS = "Include small letters (a ,b, c)"
     const val INCLUDE_NUMBERS = "Include numbers (1 ,2, 3)"
-    const val INCLUDE_SYMBOLS = "Include symbols (@ ,!, %)\""
+    const val INCLUDE_SYMBOLS = "Include symbols (@ ,!, %)"
 }
