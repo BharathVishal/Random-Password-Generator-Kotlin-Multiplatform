8fbdb76a2c3ff753c34bcc24aff8f898396e5fbb
Bharath Vishal G <bharathvishalg@gmail.com>
2025-01-18 17:22:50 +0530

    Added Screenshots

---
+++ composeApp/build/generated/compose/resourceGenerator/kotlin/commonMainResourceAccessors/randompasswordgenerator/composeapp/generated/resources/Drawable0.commonMain.kt
@@ -0,0 +1,156 @@
+@file:OptIn(org.jetbrains.compose.resources.InternalResourceApi::class)
+
+package randompasswordgenerator.composeapp.generated.resources
+
+import kotlin.OptIn
+import kotlin.String
+import kotlin.collections.MutableMap
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.InternalResourceApi
+
+private object CommonMainDrawable0 {
+  public val animation: DrawableResource by 
+      lazy { init_animation() }
+
+  public val animation_off: DrawableResource by 
+      lazy { init_animation_off() }
+
+  public val animation_on: DrawableResource by 
+      lazy { init_animation_on() }
+
+  public val baseline_password_24: DrawableResource by 
+      lazy { init_baseline_password_24() }
+
+  public val compose_multiplatform: DrawableResource by 
+      lazy { init_compose_multiplatform() }
+
+  public val dark_mode_24dp: DrawableResource by 
+      lazy { init_dark_mode_24dp() }
+
+  public val help_24dp: DrawableResource by 
+      lazy { init_help_24dp() }
+
+  public val info_24dp: DrawableResource by 
+      lazy { init_info_24dp() }
+
+  public val light_mode_24dp: DrawableResource by 
+      lazy { init_light_mode_24dp() }
+}
+
+@InternalResourceApi
+internal fun _collectCommonMainDrawable0Resources(map: MutableMap<String, DrawableResource>) {
+  map.put("animation", CommonMainDrawable0.animation)
+  map.put("animation_off", CommonMainDrawable0.animation_off)
+  map.put("animation_on", CommonMainDrawable0.animation_on)
+  map.put("baseline_password_24", CommonMainDrawable0.baseline_password_24)
+  map.put("compose_multiplatform", CommonMainDrawable0.compose_multiplatform)
+  map.put("dark_mode_24dp", CommonMainDrawable0.dark_mode_24dp)
+  map.put("help_24dp", CommonMainDrawable0.help_24dp)
+  map.put("info_24dp", CommonMainDrawable0.info_24dp)
+  map.put("light_mode_24dp", CommonMainDrawable0.light_mode_24dp)
+}
+
+internal val Res.drawable.animation: DrawableResource
+  get() = CommonMainDrawable0.animation
+
+private fun init_animation(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
+  "drawable:animation",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/randompasswordgenerator.composeapp.generated.resources/drawable/animation.xml", -1, -1),
+    )
+)
+
+internal val Res.drawable.animation_off: DrawableResource
+  get() = CommonMainDrawable0.animation_off
+
+private fun init_animation_off(): DrawableResource =
+    org.jetbrains.compose.resources.DrawableResource(
+  "drawable:animation_off",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/randompasswordgenerator.composeapp.generated.resources/drawable/animation_off.xml", -1, -1),
+    )
+)
+
+internal val Res.drawable.animation_on: DrawableResource
+  get() = CommonMainDrawable0.animation_on
+
+private fun init_animation_on(): DrawableResource =
+    org.jetbrains.compose.resources.DrawableResource(
+  "drawable:animation_on",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/randompasswordgenerator.composeapp.generated.resources/drawable/animation_on.xml", -1, -1),
+    )
+)
+
+internal val Res.drawable.baseline_password_24: DrawableResource
+  get() = CommonMainDrawable0.baseline_password_24
+
+private fun init_baseline_password_24(): DrawableResource =
+    org.jetbrains.compose.resources.DrawableResource(
+  "drawable:baseline_password_24",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/randompasswordgenerator.composeapp.generated.resources/drawable/baseline_password_24.xml", -1, -1),
+    )
+)
+
+internal val Res.drawable.compose_multiplatform: DrawableResource
+  get() = CommonMainDrawable0.compose_multiplatform
+
+private fun init_compose_multiplatform(): DrawableResource =
+    org.jetbrains.compose.resources.DrawableResource(
+  "drawable:compose_multiplatform",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/randompasswordgenerator.composeapp.generated.resources/drawable/compose-multiplatform.xml", -1, -1),
+    )
+)
+
+internal val Res.drawable.dark_mode_24dp: DrawableResource
+  get() = CommonMainDrawable0.dark_mode_24dp
+
+private fun init_dark_mode_24dp(): DrawableResource =
+    org.jetbrains.compose.resources.DrawableResource(
+  "drawable:dark_mode_24dp",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/randompasswordgenerator.composeapp.generated.resources/drawable/dark_mode_24dp.xml", -1, -1),
+    )
+)
+
+internal val Res.drawable.help_24dp: DrawableResource
+  get() = CommonMainDrawable0.help_24dp
+
+private fun init_help_24dp(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
+  "drawable:help_24dp",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/randompasswordgenerator.composeapp.generated.resources/drawable/help_24dp.xml", -1, -1),
+    )
+)
+
+internal val Res.drawable.info_24dp: DrawableResource
+  get() = CommonMainDrawable0.info_24dp
+
+private fun init_info_24dp(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
+  "drawable:info_24dp",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/randompasswordgenerator.composeapp.generated.resources/drawable/info_24dp.xml", -1, -1),
+    )
+)
+
+internal val Res.drawable.light_mode_24dp: DrawableResource
+  get() = CommonMainDrawable0.light_mode_24dp
+
+private fun init_light_mode_24dp(): DrawableResource =
+    org.jetbrains.compose.resources.DrawableResource(
+  "drawable:light_mode_24dp",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/randompasswordgenerator.composeapp.generated.resources/drawable/light_mode_24dp.xml", -1, -1),
+    )
+)
